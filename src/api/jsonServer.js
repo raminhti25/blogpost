bf0c36368e534452a7e435c724f2401471526925
raminhti <raminhti@gmail.com>
2020-07-25 19:21:06 +0430

    initial commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://5b65a9baf070.ngrok.io"
+});
---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Context } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
+
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const BlogPost = state.find(
+    blogPost => blogPost.id == navigation.getParam("id")
+  );
+
+  return (
+    <View>
+      <Text>{BlogPost.title}</Text>
+      <Text>{BlogPost.content}</Text>
+    </View>
+  );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Edit", { id: navigation.geParam("id") })
+        }
+      >
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    )
+  };
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
